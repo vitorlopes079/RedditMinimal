3c59815e9dd96533c9d45bacab7570a6a66cdc11
vitorlopes079 <vitorlopes079@gmail.com>
2023-12-23 17:16:03 +0700

    completely restyled website

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
-
-function Loading({loading}){
-    return(
-        <ClipLoader
-          color={"#36d7b7"}
-          loading={loading}
-          size={75}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-          
-          
-        />
-    )
+function Loading({ loading }) {
+  return (
+    <ClipLoader
+      color={"#ff4500"}
+      loading={loading}
+      size={75}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+  );
 }
 
 export default Loading;
